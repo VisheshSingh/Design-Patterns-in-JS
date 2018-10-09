69cf08fb1f74dd96e6d8e6e85c21626aacc4240d
Vishesh <visheshsingh16@gmail.com>
2018-10-09 10:27:35 -0400

    Revealing module pattern

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // STANDARD MODULE PATTERN
 
-const UICtrl = (function() {
+/* const UICtrl = (function() {
   let text = "May the force be with you!";
 
   function changeText() {
@@ -16,6 +16,31 @@ const UICtrl = (function() {
   };
 })();
 
-UICtrl.callChangeText();
+UICtrl.callChangeText(); */
 // console.log(UICtrl.text); // cannot access text as it is private
 // UICtrl.changeText(); // It is private and hence not accessible directly
+
+// REVEALING MODULE PATTERN
+
+const ItemCtrl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log("Item pushed...");
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
+
+  return {
+    add: add,
+    get: get
+  };
+})();
+
+ItemCtrl.add({ id: 1, product: "Lightsaber" });
+console.log(ItemCtrl.get(1));
